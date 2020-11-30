2c5c19d80b87c3e2236a72718e531be76fd4acfa
FaithZoro <2602280930@qq.com>
2020-11-30 21:12:49 +0800

    风光无限是你，跌落尘埃也是你，重点是你，而不是怎样的你

---
+++ 789/src/XL110.java
@@ -0,0 +1,16 @@
+
+public class XL110 {
+
+	private int id;
+	private String username;
+	private String password;
+	
+	public XL110(int id,String username,String password) {
+
+	this.id= id;
+	this.username= username;
+	this.password= password;
+	
+	}
+	
+}
